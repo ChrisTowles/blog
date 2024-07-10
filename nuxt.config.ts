c3a03ded52150dacabcc9fb7aa1dc2525ec6e168
Chris Towles <Chris.Towles@gmail.com>
2024-07-09 21:12:03 -0400

    chore: update nuxt config for GA

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -67,7 +67,7 @@ export default defineNuxtConfig({
 
   // set in .env with NUXT_PUBLIC_GTAG_ID
   gtag: {
-    id: 'G-XXXXXXXXXX', // place holder to be replaced with actual `.env` file value
+    id: 'G-XXX-Fake', // place holder to be replaced with actual `.env` file value
     config: { anonymize_ip: false },
   },
 
