9e7e1a28e5ebd6f8ae5518dfdd4c2487a0664a1b
Chris Towles <Chris.Towles@gmail.com>
2025-04-16 22:05:36 -0400

    chore: update home page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-
 // const { data: navigation } = await useAsyncData('navigation', () => queryCollectionNavigation('docs'))
 
 const { data: page } = await useAsyncData('index', () => queryCollection('index').first())
@@ -17,40 +16,66 @@ useSeoMeta({
 
 <template>
   <div v-if="page">
-    <UPageHero :title="page.hero.title" :description="page.hero.description" :links="page.hero.links">
+    <UPageHero
+      :title="page.hero.title"
+      :description="page.hero.description"
+      :links="page.hero.links"
+    >
       <template #top>
         <div
-          class="absolute rounded-full dark:bg-(--ui-primary) blur-[300px] size-60 sm:size-80 transform -translate-x-1/2 left-1/2 -translate-y-80" />
+          class="absolute rounded-full dark:bg-(--ui-primary) blur-[300px] size-60 sm:size-80 transform -translate-x-1/2 left-1/2 -translate-y-80"
+        />
       </template>
-
     </UPageHero>
 
-
-    <UPageSection :title="page.logos.title" />
-    <UPageMarquee :repeat="6" pause-on-hover > 
-      <ULink v-for="x in page.logos.links" as="button" :to="x.to" :key="x.label">
-        <UIcon :to="x.to" :name="x.icon" class="size-10 shrink-0" />
-      </ULink>
-
-    </UPageMarquee>
+    <USeparator />
 
     <UContainer>
-        <UPageHeader
-            class="py-[50px]"
-            title="Blog Posts"
-        />
-        <UPageBody>
-            <BlogPostList />
-        </UPageBody>
+      <UPageHeader
+        class="py-[50px]"
+        title="Blog Posts"
+      />
+      <UPageBody>
+        <BlogPostList />
+      </UPageBody>
     </UContainer>
 
-    <UPageSection :title="page.features.title" :description="page.features.description">
+    <USeparator />
+
+    <UPageSection
+      :title="page.features.title"
+      :description="page.features.description"
+    >
       <UPageGrid>
-        <UPageCard v-for="(item, index) in page.features.items" :key="index" v-bind="item" spotlight />
+        <UPageCard
+          v-for="(item, index) in page.features.items"
+          :key="index"
+          v-bind="item"
+          spotlight
+        />
       </UPageGrid>
     </UPageSection>
 
     <USeparator />
 
+    <UPageSection :title="page.logos.title" />
+    <UPageMarquee
+      :repeat="6"
+      pause-on-hover
+      class="pb-8"
+    >
+      <ULink
+        v-for="x in page.logos.links"
+        :key="x.label"
+        as="button"
+        :to="x.to"
+      >
+        <UIcon
+          :to="x.to"
+          :name="x.icon"
+          class="size-10 shrink-0"
+        />
+      </ULink>
+    </UPageMarquee>
   </div>
 </template>
