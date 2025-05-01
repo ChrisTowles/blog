05df30dc952ebc8fee961e6d18af0e6f4dc3cee7
Chris Towles <Chris.Towles@gmail.com>
2025-05-01 09:55:35 -0400

    starting mcp

--- app/composables/useHighlighter.ts
+++ app/composables/useHighlighter.ts
@@ -1,5 +1,4 @@
-import { createHighlighter, type HighlighterGeneric } from 'shiki'
-import { createJavaScriptRegexEngine } from 'shiki/engine-javascript.mjs'
+import { createHighlighter, createJavaScriptRegexEngine, type HighlighterGeneric } from 'shiki'
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 let highlighter: HighlighterGeneric<any, any> | null = null
@@ -9,6 +8,7 @@ let promise: Promise<HighlighterGeneric<any, any>> | null = null
 export const useHighlighter = async () => {
   if (!promise) {
     promise = createHighlighter({
+      // note: mermaid is not supported in shiki web bundle, and issue with full bundle.
       langs: ['vue', 'js', 'ts', 'css', 'html', 'json', 'yaml', 'markdown', 'bash', 'python'],
       themes: ['material-theme-palenight', 'material-theme-lighter'],
       engine: createJavaScriptRegexEngine()
