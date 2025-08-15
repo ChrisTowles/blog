2fd0639da4864f6ccb4b8d7369dd2e0c7a340d5b
Chris Towles <Chris.Towles@gmail.com>
2025-08-15 00:05:34 -0400

    chore: fix  nuxthub issues still v2

--- nuxt.config.ts
+++
@@ -1,6 +0,0 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-// NOTE: in the root of the repo only because `pnpx nuxthub@latest ensure` was failing in the github flow.
-
-export default defineNuxtConfig({
-  modules: ['@nuxthub/core']
-})
