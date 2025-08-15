40d5ed55969fca352d7900900503941d2e5df663
Chris Towles <Chris.Towles@gmail.com>
2025-08-14 23:47:26 -0400

    chore: fix pnpm dlx nuxthub@latest ensure

---
+++ nuxt.config.ts
@@ -0,0 +1,6 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+// NOTE: in the root of the repo only because `pnpx nuxthub@latest ensure` was failing in the github flow.
+
+export default defineNuxtConfig({
+  modules: ['@nuxthub/core']
+})
