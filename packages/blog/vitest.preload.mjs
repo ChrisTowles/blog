1ef556455ba0c092ef633614507df92fc1d50733
Chris Towles <Chris.Towles@gmail.com>
2026-01-04 22:15:33 -0500

    fix: resolve Vitest 4 deprecation warning for Nuxt environment

---
+++ packages/blog/vitest.preload.mjs
@@ -0,0 +1,13 @@
+// Preload script to suppress @nuxt/test-utils deprecation warning from 2026-01-04
+// Must run before vitest loads to catch environment init warnings
+// See: https://github.com/nuxt/test-utils/issues/1482
+// TODO: Remove when @nuxt/test-utils fixes the transformMode deprecation
+
+const originalWarn = console.warn
+console.warn = (...args) => {
+    const msg = args[0]
+    if (typeof msg === 'string' && msg.includes('transformMode') && msg.includes('deprecated')) {
+        return
+    }
+    originalWarn.apply(console, args)
+}
